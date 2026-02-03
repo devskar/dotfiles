16c9dae34785076fda6640511f63d6f859c3550f
oskar <ooskar@gmx.de>
2026-02-03 21:59:52 +0100

    feat: add antidote

--- bootstrap/linux.sh
+++ bootstrap/linux.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
-sudo apt install tmux
-sudo apt install neovim
-sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+apt update
+apt install software-properties-common -y
+add-apt-repository ppa:neovim-ppa/unstable -y
+apt install tmux -y
+apt install neovim -y
+apt install curl -y
+apt install git -y
+apt install build-essential -y
+apt install zsh -y
+sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- -y
--- install.sh
+++ install.sh
@@ -3,17 +3,22 @@ set -e
 
 echo "Installing dotfiles..."
 
+mkdir -p ~/.config
+
 ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
 ln -sf ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/starship/starship.toml ~/.config/starship.toml
+ln -sf ~/dotfiles/zsh/.zsh_plugins.txt ~/.zsh_plugins.txt
 ln -sfn ~/dotfiles/nvim ~/.config/nvim
 
 # ---- OS-specific-tweaks
 case "$OSTYPE" in
   darwin*) source ~/dotfiles/bootstrap/macos.sh ;;
-  linux*)  source ~/dotfiles/bootstrap/wsl.sh ;;
+  linux*)  source ~/dotfiles/bootstrap/linux.sh ;;
 esac
+
+git clone --depth=1 https://github.com/mattmc3/antidote.git ~/.antidote
 
 echo "Installed dotfiles!"
 
-
+source ~/.zshrc
