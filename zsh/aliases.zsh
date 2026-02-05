d883b24ab93e9d89cce57e23c4113f5564f4ad59
Pyenb <30316592+Pyenb@users.noreply.github.com>
2026-02-05 22:34:20 +0100

    chore: add trash cli alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,8 +3,8 @@ alias ..='cd ..'
 alias ...='cd ../..'
 
 # ---- ls replacements ----
-alias ll='ls -lah'
-alias ls='ls -l'
+alias ll='ls -lah --color=auto'
+alias ls='ls -l --color=auto'
 
 # ---- NeoVim -----
 alias vim='nvim'
@@ -13,4 +13,8 @@ alias vim='nvim'
 alias t='tmux'
 alias ta='tmux attach -t'
 alias tn='tmux new -s'
+
+# ---- trash cli ----
+alias rm='trash'
+alias 'rm -rf'='trash -r'
 
