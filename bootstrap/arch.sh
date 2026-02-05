c1c9db4c37a3428e02149c871101755f6794eedd
Pyenb <30316592+Pyenb@users.noreply.github.com>
2026-02-05 22:33:50 +0100

    feat: add arch install script

---
+++ bootstrap/arch.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+set -eou pipefail
+
+if [[ "$EUID" -eq 0 ]]; then
+  SUDO=""
+else
+  SUDO="sudo"
+fi
+
+# Update system
+$SUDO pacman -Syu --noconfirm
+
+# Common
+$SUDO pacman -S base-devel --noconfirm
+$SUDO pacman -S curl --noconfirm
+$SUDO pacman -S unzip --noconfirm
+$SUDO pacman -S git --noconfirm
+$SUDO pacman -S ripgrep --noconfirm
+$SUDO pacman -S trash-cli --noconfirm
+$SUDO pacman -S man-db --noconfirm
+$SUDO pacman -S man-pages --noconfirm
+
+# Node
+$SUDO pacman -S nodejs npm --noconfirm
+
+# Language
+$SUDO pacman -S glibc --noconfirm
+$SUDO sed -i 's/#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
+$SUDO locale-gen
+
+# Tmux
+$SUDO pacman -S tmux --noconfirm
+
+# Nvim
+$SUDO pacman -S neovim --noconfirm
+
+# zsh
+$SUDO pacman -S zsh --noconfirm
+chsh -s /bin/zsh
+
+sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- -y
