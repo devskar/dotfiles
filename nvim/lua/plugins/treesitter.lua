1369123896b3b2bc59706077825ecf23d79bc99a
oskar <ooskar@gmx.de>
2026-02-13 14:35:43 +0100

    feat: add markdown to treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,14 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    lazy = false,
-    config = function()
-      require("nvim-treesitter").setup({
-        ensure_installed = { "lua", "javascript", "html", "c_sharp" },
-        sync_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },
-        auto_install = true
-      })
-    end
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  lazy = false,
+  config = function()
+    require("nvim-treesitter").setup({
+      ensure_installed = { "lua", "javascript", "html", "c_sharp", "markdown" },
+      sync_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },
+      auto_install = true
+    })
+  end
 }
