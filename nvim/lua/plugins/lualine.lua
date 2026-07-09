e395cafb3118ff4a02bc74b1e2de0a93882f775d
oskar <ooskar@gmx.de>
2026-07-09 11:45:12 +0200

    feat(nvim): lua-line show realtive path

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,3 +1,7 @@
+local function project_filename()
+  return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":.")
+end
+
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -6,6 +10,9 @@ return {
     require("lualine").setup({
       options = {
         theme = "tomorrow_night",
+      },
+      sections = {
+        lualine_c = { project_filename },
       },
     })
   end,
