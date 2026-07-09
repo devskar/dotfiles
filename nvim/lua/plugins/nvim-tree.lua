872e37208351ef550820b33f92edb7f31d7e2fc6
oskar <ooskar@gmx.de>
2026-07-09 11:04:24 +0200

    feat(nvim): nvim-tree sitter no floating window

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -9,9 +9,6 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
-    local HEIGHT_RATIO = 0.8
-    local WIDTH_RATIO = 0.5
-
     require("nvim-tree").setup({
       sync_root_with_cwd = true,
       update_focused_file = {
@@ -19,33 +16,8 @@ return {
         update_root = true,
       },
       view = {
-        float = {
-          enable = true,
-          open_win_config = function()
-            local screen_w = vim.o.columns
-            local screen_h = vim.o.lines - vim.o.cmdheight
-
-            local window_w = math.floor(screen_w * WIDTH_RATIO)
-            local window_h = math.floor(screen_h * HEIGHT_RATIO)
-
-            local center_x = (screen_w - window_w) / 2
-            local center_y = (vim.o.lines - window_h) / 2 - vim.o.cmdheight
-
-            return {
-              border = "rounded",
-              relative = "editor",
-              row = center_y,
-              col = center_x,
-              width = window_w,
-              height = window_h,
-            }
-          end,
-        },
-
-        width = function()
-          return math.floor(vim.o.columns * WIDTH_RATIO)
-        end,
-      },
+        side = "right"
+      }
     })
 
     -- Close nvim-tree cleanly when quitting Neovim
