4aced4c4bdb22ad5f1c1ced5c0cab4f90004dc09
oskar <ooskar@gmx.de>
2026-07-13 19:33:41 +0200

    feat: add vim-tmux-navigator

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -36,6 +36,13 @@ map("n", "<leader>fh", function()
 end, { desc = "Help" })
 map("n", "<leader>fp", "<cmd>lua require'telescope'.extensions.project.project{}<CR>", { desc = "Projects" })
 
+-- ---- Tmux Navigator ----
+map("n", "<c-h>", "<cmd>TmuxNavigateLeft<cr>")
+map("n", "<c-j>", "<cmd>TmuxNavigateDown<cr>")
+map("n", "<c-k>", "<cmd>TmuxNavigateUp<cr>")
+map("n", "<c-l>", "<cmd>TmuxNavigateRight<cr>")
+map("n", "<c-\\>", "<cmd>TmuxNavigatePrevious<cr>")
+
 -- ---- LSP ----
 vim.api.nvim_create_autocmd(
   "LspAttach",
@@ -47,20 +54,20 @@ vim.api.nvim_create_autocmd(
       -- Buffer local mappings.
       -- See `:help vim.lsp.*` for documentation on any of the below functions
       local opts = { buffer = ev.buf }
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-      vim.keymap.set("n", "<leader><space>", vim.lsp.buf.hover, opts)
-      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-      vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
-      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-      vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+      map("n", "gd", vim.lsp.buf.definition, opts)
+      map("n", "<leader><space>", vim.lsp.buf.hover, opts)
+      map("n", "gi", vim.lsp.buf.implementation, opts)
+      map("n", "<leader>D", vim.lsp.buf.type_definition, opts)
+      map("n", "<leader>rn", vim.lsp.buf.rename, opts)
+      map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+      map("n", "gr", vim.lsp.buf.references, opts)
 
-      vim.keymap.set("n", "<leader>f", function()
+      map("n", "<leader>f", function()
         vim.lsp.buf.format({ async = true })
       end, opts)
 
       -- Open the diagnostic under the cursor in a float window
-      vim.keymap.set("n", "<leader>d", function()
+      map("n", "<leader>d", function()
         vim.diagnostic.open_float({
           border = "rounded",
         })
